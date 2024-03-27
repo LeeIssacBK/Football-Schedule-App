f8f228359b21f412fc427cfb76ccb4e94b02fc8e
issac <qodrp@naver.com>
2024-03-27 12:12:24 +0900

    learn flutter design 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '이건 안 보이네?',
-      theme: ThemeData.light().copyWith(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue
-        )
-      ),
-      home: const MyHomePage(),
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: const Text('scaffold title'),
-        titleTextStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-        ),
+        title: const Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
         centerTitle: true,
-        backgroundColor: Colors.redAccent,
         elevation: 0.0,
       ),
-      body: const Center(
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Hello'),
-              Text('Hello'),
-              Text('Hello'),
-            ],
-          ),
-      )
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 
 }

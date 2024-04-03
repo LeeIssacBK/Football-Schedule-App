b110561ba6afba9279dc79ed1a6140ba85acad91
issac <qodrp@naver.com>
2024-04-03 14:31:41 +0900

    add app splash screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:geolpo/main.dart';
+import 'package:geolpo/study/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

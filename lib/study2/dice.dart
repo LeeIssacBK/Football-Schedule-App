6d30de5c3679c1790fb31c9a1911c9aa8a84a441
issac <qodrp@naver.com>
2024-04-05 10:26:43 +0900

    add app dice game

--- lib/study2/dice.dart
+++ lib/study2/dice.dart
@@ -1,9 +1,65 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+
+  int leftDice = 1;
+  int rightDice = 1;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+        backgroundColor: Colors.redAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text('Dice game'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Image.asset('image/dice$leftDice.png'),
+                      // flex: 2,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Expanded(
+                      child: Image.asset('image/dice$rightDice.png'),
+                      // flex: 1,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 60.0,
+              ),
+              ElevatedButton(
+                  child: Icon(Icons.play_arrow,
+                      color: Colors.white, size: 50.0),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(100, 50),
+                      backgroundColor: Colors.orangeAccent),
+                  onPressed: (){
+                    setState(() {
+                      leftDice = Random().nextInt(6) + 1;
+                      rightDice = Random().nextInt(6) + 1;
+                    });
+                  },
+              )
+            ],
+          ),
+        ));
   }
-  
 }
--- lib/study2/login_dice.dart
+++ lib/study2/login_dice.dart
@@ -44,62 +44,68 @@ class _LogInState extends State<LogIn> {
       ),
       body: Builder(
         builder: (context) {
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(padding: EdgeInsets.only(top: 50)),
-                Center(
-                  child: Image(
-                    image: AssetImage('image/saedaegal_party.gif'),
-                    width: 190.0,
-                    height: 190.0,
-                  ),
-                ),
-                Form(
-                    child: Theme(
-                  data: ThemeData(
-                      primaryColor: Colors.teal,
-                      inputDecorationTheme: InputDecorationTheme(
-                          labelStyle:
-                              TextStyle(color: Colors.teal, fontSize: 15.0))),
-                  child: Container(
-                    padding: EdgeInsets.all(40.0),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: controller,
-                          decoration:
-                              InputDecoration(labelText: 'Enter "dice"'),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        TextField(
-                          controller: controller2,
-                          decoration:
-                              InputDecoration(labelText: 'Enter Password'),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        SizedBox(height: 40.0),
-                        ElevatedButton(
-                          child: Icon(Icons.arrow_forward,
-                              color: Colors.white, size: 35.0),
-                          style: ElevatedButton.styleFrom(
-                              minimumSize: Size(100, 50),
-                              backgroundColor: Colors.orangeAccent),
-                          onPressed: () {
-                            if (isPass(controller, controller2)) {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (_) => Dice()));
-                            } else {
-                              showSnackBar(context);
-                            }
-                          },
-                        )
-                      ],
+          return GestureDetector(
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  Center(
+                    child: Image(
+                      image: AssetImage('image/saedaegal_party.gif'),
+                      width: 190.0,
+                      height: 190.0,
                     ),
                   ),
-                )),
-              ],
+                  Form(
+                      child: Theme(
+                    data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: InputDecorationTheme(
+                            labelStyle:
+                                TextStyle(color: Colors.teal, fontSize: 15.0))),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            autofocus: true,
+                            controller: controller,
+                            decoration:
+                                InputDecoration(labelText: 'Enter "dice"'),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            controller: controller2,
+                            decoration:
+                                InputDecoration(labelText: 'Enter Password'),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 40.0),
+                          ElevatedButton(
+                            child: Icon(Icons.arrow_forward,
+                                color: Colors.white, size: 35.0),
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: Size(100, 50),
+                                backgroundColor: Colors.orangeAccent),
+                            onPressed: () {
+                              if (isPass(controller, controller2)) {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (_) => Dice()));
+                              } else {
+                                showSnackBar(context);
+                              }
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+                ],
+              ),
             ),
           );
         },

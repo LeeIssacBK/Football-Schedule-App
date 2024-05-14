d81ad0de00d532656d14bd8401609174ce38b02a
issac <qodrp@naver.com>
2024-05-14 19:19:45 +0900

    add alert list

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 import 'dto/alert.dart';
+import 'dto/fixture.dart';
 import 'global.dart';
 
 class Alarm extends StatefulWidget {
@@ -21,6 +23,7 @@ class _AlarmState extends State<Alarm> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
@@ -43,7 +46,8 @@ class _AlarmState extends State<Alarm> {
                   )
               ),
               Container(
-                child: getList(),
+                padding: const EdgeInsets.all(5.0),
+                child: getList(screenWidth),
               )
             ],
           ),
@@ -52,7 +56,7 @@ class _AlarmState extends State<Alarm> {
     );
   }
 
-  Widget getList() {
+  Widget getList(double screenWidth) {
     return FutureBuilder<List<Alert>>(
       future: getAlerts(),
       builder: (context, snapshot) {
@@ -82,11 +86,143 @@ class _AlarmState extends State<Alarm> {
         }
         return Column(
           children: alerts.map((alert) {
-              return Column(
-                children: [
-                  Text('${alert.fixture.home!.name}'),
-                  Text('${alert.fixture.away!.name}'),
-                ],
+            Fixture fixture = alert.fixture;
+              return Container(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: screenWidth * 0.4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            fixture.league!.name,
+                            style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            getKoreanRound(fixture.round),
+                            style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat('y. M. d ${getKoreanWeekDay(fixture.date)} HH:mm')
+                                .format(fixture.date),
+                            style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                              fixture.home!.stadium,
+                              style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                              overflow: TextOverflow.ellipsis
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: screenWidth * 0.44,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Image.network(fixture.home!.logo, height: 50,),
+                          ),
+                          const Text('vs'),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Image.network(fixture.away!.logo, height: 50,),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: screenWidth * 0.1,
+                      child: IconButton(
+                          onPressed: (){
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('알람 추가',
+                                      style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, color: Colors.indigo),
+                                    ),
+                                    contentTextStyle: const TextStyle(color: Colors.indigo, fontWeight: FontWeight.bold, fontSize: 15.0),
+                                    content: Container(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.all(5.0),
+                                                child: Image.network(fixture.league!.logo, height: 30,),
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.all(5.0),
+                                                child: Text('${fixture.league!.name} ${getKoreanRound(fixture.round)}'),
+                                              )
+                                            ],
+                                          ),
+                                          Text('${fixture.home!.name} vs ${fixture.away!.name} 경기를 알람 설정 하시겠습니까?'),
+                                        ],
+                                      ),
+                                    ),
+                                    actions: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Padding(
+                                              padding: const EdgeInsets.all(5.0),
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                  // saveAlert(fixture.apiId).then((flag) => {
+                                                  //   if (flag) {
+                                                  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                                  //       content: Text('알람 등록이 완료되었습니다!',
+                                                  //         textAlign: TextAlign.center,
+                                                  //         style: TextStyle(color: Colors.white),
+                                                  //       ),
+                                                  //       backgroundColor: Colors.teal,
+                                                  //       duration: Duration(milliseconds: 1000),
+                                                  //     ))
+                                                  //   } else {
+                                                  //     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                                  //       content: Text('알람 등록이 실패하였습니다. 다시 시도해주세요.',
+                                                  //         textAlign: TextAlign.center,
+                                                  //         style: TextStyle(color: Colors.white),
+                                                  //       ),
+                                                  //       backgroundColor: Colors.redAccent,
+                                                  //       duration: Duration(milliseconds: 1000),
+                                                  //     ))
+                                                  //   }
+                                                  // });
+                                                },
+                                                child: const Text('예'),
+                                              )
+                                          ),
+                                          Padding(
+                                              padding: const EdgeInsets.all(5.0),
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: const Text('아니오'),
+                                              )
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  );
+                                });
+                          }, icon: fixture.isAlert ?
+                      const Icon(Icons.alarm_on, color: Colors.teal,) : const Icon(Icons.add_alarm)
+                      ),
+                    )
+                  ],
+                ),
               );
             }).toList()
         );

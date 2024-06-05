19fc04e68c5cc6c74f4971f50f3a62f9ac8201b2
issac <qodrp@naver.com>
2024-06-05 18:55:25 +0900

    add standing dto

---
+++ lib/dto/standing_dto.dart
@@ -0,0 +1,90 @@
+
+
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Standing {
+
+  int rank;
+  int points;
+  int goalsDiff;
+  String group;
+  String form;
+  String status;
+  String description;
+  Game all;
+  Game home;
+  Game away;
+
+  Standing({
+    required this.rank,
+    required this.points,
+    required this.goalsDiff,
+    required this.group,
+    required this.form,
+    required this.status,
+    required this.description,
+    required this.all,
+    required this.home,
+    required this.away,
+  });
+
+  factory Standing.fromJson(Map<dynamic, dynamic> json) {
+    return Standing(
+      rank: json['_rank'],
+      points: json['points'],
+      goalsDiff: json['goalsDiff'],
+      group: json['group'],
+      form: json['form'],
+      status: json['_status'],
+      description: json['description'],
+      all: Game.fromJson(json['_all']),
+      home: Game.fromJson(json['home']),
+      away: Game.fromJson(json['away']),
+    );
+  }
+  
+}
+
+class Game {
+
+  int played;
+  int win;
+  int draw;
+  int lose;
+  Goal goals;
+
+  Game({
+    required this.played,
+    required this.win,
+    required this.draw,
+    required this.lose,
+    required this.goals,
+  });
+
+  factory Game.fromJson(Map<dynamic, dynamic> json) {
+    return Game(
+      played: json['played'],
+      win: json['win'],
+      draw: json['draw'],
+      lose: json['lose'],
+      goals: json['goals'],
+    );
+  }
+
+}
+
+class Goal {
+  int for_;
+  int against;
+
+  Goal({required this.for_, required this.against});
+
+  factory Goal.fromJson(Map<dynamic, dynamic> json) {
+    return Goal(
+      for_: json['_for'],
+      against: json['against'],
+    );
+  }
+
+}

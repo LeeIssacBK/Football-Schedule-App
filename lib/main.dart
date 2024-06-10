1619e9e26b5a72b20cb0756fad0c99efef8551cc
issac <qodrp@naver.com>
2024-06-10 18:26:37 +0900

    add firebase messaging setting ios, android

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:syncfusion_localizations/syncfusion_localizations.dart';
-import 'dart:async';
-import 'login.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+
+import 'firebase_options.dart';
+import 'login.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+String? fcmToken;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  fcmToken = await FirebaseMessaging.instance.getToken();
+  print('fcmToken\n$fcmToken');
   KakaoSdk.init(nativeAppKey: '6ef65107292ada731123dac02c2d72bc');
   runApp(App());
 }
